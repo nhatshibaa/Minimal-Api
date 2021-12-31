679ae29b103c6c0cd9ee0586aebe009139a8a578
Nhật Nguyễn Tiến <kaotuong113@gmail.com>
2021-12-31 16:06:29 +0700

    eap-practice

---
+++ EAP-Practice/Data/EAP_PracticeContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EAP_Practice.Models;
+
+namespace EAP_Practice.Data
+{
+    public class EAP_PracticeContext : DbContext
+    {
+        public EAP_PracticeContext (DbContextOptions<EAP_PracticeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EAP_Practice.Models.Employee> Employee { get; set; }
+    }
+}
---
+++ EAP-Practice/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace EAP_Practice.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+       
+    }
+}
